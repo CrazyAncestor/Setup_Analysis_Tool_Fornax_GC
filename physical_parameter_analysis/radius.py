5cb5c7c415ba085202b198b251417a6550de4f36
Albert Wu <38256449+CrazyAncestor@users.noreply.github.com>
2021-07-12 17:57:23 +0800

    Update radius.py

--- physical_parameter_analysis/radius.py
+++ physical_parameter_analysis/radius.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 
 idx_start = 68
-idx_end = 100
+idx_end = 68
 filein  = [ 'Data_%06d'%idx for idx in range(idx_start, idx_end+1) ] 
 
 nbin    = 50
@@ -21,25 +21,32 @@ for i in range(0, len(filein)):
    def gc_star( pfilter, data ):
       filter = (data[ "all", "particle_mass" ] >7.1e35)
       return filter
-   
+
+   def halo_star( pfilter, data ):
+      filter = (data[ "all", "particle_mass" ] <7.1e35)
+      return filter
 
    add_particle_filter( "gc_star", function=gc_star, filtered_type="all", requires=["particle_mass"] )
-   
-   ds.add_particle_filter( "gc_star" )
-   
+   add_particle_filter( "halo_star", function=halo_star, filtered_type="all", requires=["particle_mass"] )
 
+   ds.add_particle_filter( "gc_star" )
+   df.add_particle_filter( "halo_star" )
 
    # get the mass and creation time of the new stars
    ad            = ds.all_data()
-   x = np.array(ad[ "gc_star", "particle_position_x" ].in_units( "kpc" ) )
-   y = np.array(ad[ "gc_star", "particle_position_y" ].in_units( "kpc" ) )
-   z = np.array(ad[ "gc_star", "particle_position_z" ].in_units( "kpc" ) )
-   centre = np.array([np.average(ad[ "gc_star", "particle_position_x" ].in_units( "kpc" ) ),
+   af            = df.all_data()
+   x_gc = np.array(ad[ "gc_star", "particle_position_x" ].in_units( "kpc" ) )
+   y_gc = np.array(ad[ "gc_star", "particle_position_y" ].in_units( "kpc" ) )
+   z_gc = np.array(ad[ "gc_star", "particle_position_z" ].in_units( "kpc" ) )
+   centre_gc = np.array([np.average(ad[ "gc_star", "particle_position_x" ].in_units( "kpc" ) ),
                       np.average(ad[ "gc_star", "particle_position_y" ].in_units( "kpc" ) ),
                       np.average(ad[ "gc_star", "particle_position_z" ].in_units( "kpc" ) )])
+   x_halo = np.array(af[ "halo_star", "particle_position_x" ].in_units( "kpc" ) )
+   y_halo = np.array(af[ "halo_star", "particle_position_y" ].in_units( "kpc" ) )
+   z_halo = np.array(af[ "halo_star", "particle_position_z" ].in_units( "kpc" ) )
+   centre_halo = np.array([np.average(af[ "halo_star", "particle_position_x" ].in_units( "kpc" ) ),
+                      np.average(af[ "halo_star", "particle_position_y" ].in_units( "kpc" ) ),
+                      np.average(af[ "halo_star", "particle_position_z" ].in_units( "kpc" ) )])
    
    
-   radius = np.sum((centre-np.array([10,10,10]))**2)**0.5
-   print(str(radius)+',')
-   
-   
+   radius = np.sum((centre_gc-centre_halo)**2)**0.5
